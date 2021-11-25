5f74559ed1112489e38567ef339fb1438afb8112
Sean Mizen <smizzle-probably@github.com>
2021-11-25 16:59:19 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tourguide.ar',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
